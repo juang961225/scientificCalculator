08f06f2f3670334931a3094516ade7f8ef0e8714
juang961225 <juang961225@gmail.com>
2020-07-07 20:17:09 -0500

    first commit develop

---
+++ scientificCalculator.py
@@ -0,0 +1,1 @@
+# crear una calculadora cientifica que vaya dentro de un while y tenga las cuatro operaciones basicas (+.-,/,*) y las trigonometricas de cualquier numero (sen,cos,tan) tambien radicacion y potenciacion
